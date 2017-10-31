a154574ff503950bfa23abcf1bc206c6ffc71d5d
fernycorral <fernycorral@live.com.mx>
2017-10-31 13:13:29 -0700

    BrowserCommented

--- AutomationSeleniumTemplate/AutomationSeleniumTemplate/Browser/SeleniumBrowser.cs
+++ AutomationSeleniumTemplate/AutomationSeleniumTemplate/Browser/SeleniumBrowser.cs
@@ -13,6 +13,7 @@ namespace AutomationSeleniumTemplate.Browser
 	public class SeleniumBrowser
 	{
 		IWebDriver driver;
+		private string homeWindowName;
 
 		public object ProxyType { get; private set; }
 
@@ -22,19 +23,58 @@ namespace AutomationSeleniumTemplate.Browser
 			
 			FirefoxProfile myprofile = new FirefoxProfileManager().GetProfile("mySeleniumProject");
 			driver = new FirefoxDriver(myprofile);
+			homeWindowName = driver.WindowHandles.Last();
 			
 		}
 
+		/// <summary>
+		/// Wait until specific element appear
+		/// </summary>
+		/// <param name="xpath">desired element xpath</param>
+		public void WaitUntilElementAppears(string xpath)
+		{
+			IWebElement element = (new WebDriverWait(driver, TimeSpan.FromMilliseconds(30000))
+				.Until(ExpectedConditions.ElementToBeClickable(By.XPath(xpath))));
+		}
+		/// <summary>
+		/// Find element by ID attribute and click it
+		/// </summary>
+		/// <param name="id"></param>
 		public void DoClickById(string id)
 		{
 			driver.FindElement(By.Id(id)).Click();
 		}
 
+		/// <summary>
+		/// Go to specific URL
+		/// </summary>
+		/// <param name="url"></param>
 		public void GoToURL(string url)
 		{
 			driver.Url = url;	
 		}
 
+		/// <summary>
+		/// Change to new opened window
+		/// </summary>
+		public void SwitchToNewWindow()
+		{
+			string windowName = driver.WindowHandles.Last();
+			driver.SwitchTo().Window(windowName);
+		}
+		/// <summary>
+		/// Get back to main window 
+		/// </summary>
+		public void ReturnToMainWindow()
+		{
+			driver.Close();
+			driver.SwitchTo().Window(homeWindowName);
+		}
+
+		/// <summary>
+		/// Do Click using Xpath
+		/// </summary>
+		/// <param name="XPath">Xpath where you want to click</param>
 		public void DoClickByXPath(string XPath)
 		{
 			try
@@ -45,16 +85,31 @@ namespace AutomationSeleniumTemplate.Browser
 			{ }
 		}
 
+		/// <summary>
+		/// Write text over input box
+		/// </summary>
+		/// <param name="Xpath">input XPATH</param>
+		/// <param name="text">text to be wrote</param>
 		public void SetText(string Xpath,string text)
 		{
 			driver.FindElement(By.XPath(Xpath)).SendKeys(text);
 		}
 
+		/// <summary>
+		/// Retrieve text from an element
+		/// </summary>
+		/// <param name="element">Element</param>
+		/// <returns></returns>
 		public string GetTextElement(IWebElement element)
 		{
 			return (string)(driver as IJavaScriptExecutor).ExecuteScript("return arguments[0].innerText", element);
 		}
 
+		/// <summary>
+		/// Click and wait until a specific element appears
+		/// </summary>
+		/// <param name="xpath">Desired element xpath</param>
+		/// <param name="wantedElementXpath">Wanted element xpath</param>
 		public void ClickUntilElementAppears(string xpath, string wantedElementXpath)
 		{
 			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(30000));
@@ -62,36 +117,69 @@ namespace AutomationSeleniumTemplate.Browser
 			wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(wantedElementXpath)));
 		}
 
+		/// <summary>
+		/// Find multiple elements using same xpath
+		/// </summary>
+		/// <param name="xpath">xpath to match</param>
+		/// <returns></returns>
 		public IList<IWebElement> FindElements(string xpath)
 		{
 			return driver.FindElements(By.XPath(xpath)); ;
 		}
+		/// <summary>
+		/// Find element using xpath
+		/// </summary>
+		/// <param name="xpath">Desired element xpath</param>
+		/// <returns></returns>
 		public IWebElement FindElement(string xpath)
 		{
 			return driver.FindElement(By.XPath(xpath)); ;
 		}
-
+		/// <summary>
+		/// Let driver waite for some seconds
+		/// </summary>
+		/// <param name="seconds">seconds to wait</param>
 		public void Wait(int seconds)
 		{
 			driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(seconds));
 		}
 
+		public String GetComboBoxSelectedVal(string xpath)
+		{
+			return new SelectElement(driver.FindElement(By.XPath(xpath))).SelectedOption.Text;
+		}
+
+		/// <summary>
+		/// Picks an option from a combobox
+		/// </summary>
+		/// <param name="xpath">combobox xpath</param>
+		/// <param name="textOption">option wanted</param>
 		public void SelectFromCombo(string xpath,string textOption)
 		{
-			new SelectElement(driver.FindElement(By.XPath(xpath))).AllSelectedOptions
+			new SelectElement(driver.FindElement(By.XPath(xpath))).Options
 				.First(E => E.Text == textOption).Click();
 		}
+
+		
 
 		/// <summary>
 		/// Clicks an element.
 		/// </summary>
 		/// <param name="elem">The element.</param>
-		private void click(IWebElement elem)
+		public void Click(IWebElement elem)
 		{
 			if (elem.Displayed)
 				elem.Click();
 			else
 				(driver as IJavaScriptExecutor).ExecuteScript("arguments[0].click()", elem);
+		}
+
+		public void Close()
+		{
+			driver.Close();
+			driver.Dispose();
+			
+			
 		}
 
 	}
--- AutomationSeleniumTemplate/AutomationSeleniumTemplate/TestCases/TestClass.cs
+++ AutomationSeleniumTemplate/AutomationSeleniumTemplate/TestCases/TestClass.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutomationSeleniumTemplate.Browser;
+using System.Threading;
 
 namespace AutomationSeleniumTemplate.TestCases
 {
@@ -19,6 +20,7 @@ namespace AutomationSeleniumTemplate.TestCases
 			browser.GoToURL("https://www.expedia.com/");
 		}
 		[Test]
+		[Category("a")]
 		public void Test1()
 		{
 			
@@ -31,16 +33,40 @@ namespace AutomationSeleniumTemplate.TestCases
 			browser.DoClickByXPath("//button[@data-month='10' and @data-day='24']");
 			browser.ClickUntilElementAppears("//label/button[@type='submit']", "//input[@id='stopFilter_stops-2']");
 			browser.DoClickById("stopFilter_stops-2");
-
-
-
-
-
+			Assert.AreEqual("Price (Lowest)",browser.GetComboBoxSelectedVal("//select[@name='sort']"));
 		}
+
+		[Test]
+		[Category("b")]
+		public void Test2()
+		{
+			browser.SetText("//input[@id='package-origin-hp-package']", "Chihuahua, Chihuahua, Mexico (CUU-General Roberto Fierro Villalobos Intl.)");
+			browser.SetText("//input[@id='package-destination-hp-package']", "Barcelona, Spain (BCN-All Airports)");
+			browser.DoClickById("package-departing-hp-package");
+			browser.DoClickByXPath("//button[@data-month='10' and @data-day='22']");
+			browser.DoClickById("package-returning-hp-package");
+			browser.DoClickByXPath("//button[@data-month='10' and @data-day='24']");
+			browser.ClickUntilElementAppears("//label/button[@id='search-button-hp-package']", "//div[@class='imgLoading']");
+			browser.WaitUntilElementAppears("//input[@id='star5']");
+			browser.ClickUntilElementAppears("//input[@id='star5']", "//div[@id='legal-disclosure']");
+			foreach (var elem in browser.FindElements("//article/div/div/a"))
+			{
+				elem.Click();
+				Thread.Sleep(7000);
+				browser.SwitchToNewWindow();
+				browser.DoClickById("tab-reviews");
+				browser.SelectFromCombo("//select[@id='reviews-sort-selector']", "Lowest rating");
+				string rate = browser.FindElement("//span[@class='rating']/span").Text;
+				if (Convert.ToInt32(rate) < 4)
+					Assert.Fail();
+
+			}
+		}
+
 		[TearDown]
 		public void TearDown()
 		{
-
+			browser.Close();
 		}
 	}
 }
